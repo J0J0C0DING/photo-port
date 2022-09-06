7a1c4cc2e6e7d7c37fe3b22e67c8b69d052901ea
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-09-06 16:55:45 -0500

    Contact form now has hooks to capture user input onChange

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
-  <section>
-    <h1>Contact Me</h1>
-    <form id="contact-form">
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </section>;
-}
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" defaultValue={formState.name} onChange={handleChange} name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" defaultValue={formState.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            defaultValue={formState.message}
+            rows="5"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
 export default ContactForm;
